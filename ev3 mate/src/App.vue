f9436289848aa97c2d13ee2c68d9cdb04e46b6a2
keepscode-snow <jorge.dominguez06@inacapmail.cl>
2024-11-30 21:59:59 -0300

    Add files via upload

---
+++ ev3 mate/src/App.vue
@@ -0,0 +1,26 @@
+n<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue';
+</script>
+
+<template>
+  <div>
+    <Calculator />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+/* Puedes agregar estilos adicionales aquí si es necesario */
+</style>
---
+++ ev3 mate/src/components/CalculatorDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="calculator-display">
+    {{ value }}
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  value: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+.calculator-display {
+  background: #222;
+  color: #fff;
+  font-size: 2em;
+  padding: 0.5em;
+  text-align: right;
+}
+</style>
